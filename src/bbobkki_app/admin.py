07a5cd4072a4655c58492f288b7a291df3c99318
TaeHyoungKwon <kwon5604@naver.com>
2017-11-11 02:52:20 +0900

    Making Model Class and Register admin site

--- src/bbobkki_app/admin.py
+++ src/bbobkki_app/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+"""
+You should write docstring here about models.py
+"""
+
+from django.contrib import admin
+from .models import GuessNumbers
+
+admin.site.register(GuessNumbers)
--- src/bboobkki/settings.py
+++ src/bboobkki/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'bbobkki_app',
+    'bbobkki_app',
 ]
 
 MIDDLEWARE = [
